d25f721e4a83db7b2a9cf7246334acfd3f5b196e
scbonner <codingbootcamp918@gmail.com>
2020-07-05 17:27:51 -0400

    "updating files"

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,78 +1,78 @@
-import React from 'react';
+// import React from 'react';
 
 
 
-// goal setup user interface survey form
-// form will have various formated questions 
-// creat div to handle the questions 
-// provide method to display text in form
+// // goal setup user interface survey form
+// // form will have various formated questions 
+// // creat div to handle the questions 
+// // provide method to display text in form
 
 
-// //Form component
-class ContactForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: '',
-        email:''
-      };
+// // //Form component
+// class ContactForm extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         name: '',
+//         email:''
+//       };
   
-      this.handleChange = this.handleInputChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
+//       this.handleChange = this.handleInputChange.bind(this);
+//       this.handleSubmit = this.handleSubmit.bind(this);
+//     }
     
-    handleInputChange(event) {
-      const target = event.target;
-      const value = target.type === 'checkbox' ? target.checked : target.value;
-      const name = target.name;
+//     handleInputChange(event) {
+//       const target = event.target;
+//       const value = target.type === 'checkbox' ? target.checked : target.value;
+//       const name = target.name;
       
-      this.setState({
-        [name]: value
-      });
-      console.log('Change detected. State updated' + name + ' = ' + value);
-    }
+//       this.setState({
+//         [name]: value
+//       });
+//       console.log('Change detected. State updated' + name + ' = ' + value);
+//     }
   
-    handleSubmit(event) {
-      alert('A form was submitted: ' + this.state.name + ' // ' + this.state.email);
-      event.preventDefault();
-    }
+//     handleSubmit(event) {
+//       alert('A form was submitted: ' + this.state.name + ' // ' + this.state.email);
+//       event.preventDefault();
+//     }
   
-    render() {
-      return (
-        <div>
-          <form onSubmit={this.handleSubmit} >
-            <div className="form-group">
-              <label for="nameImput">Name</label>
-              <input type="text" name="name" value={this.state.name} onChange={this.handleChange} className="form-control" id="nameImput" placeholder="Name" />
-            </div>
-            <div className="form-group">
-              <label for="emailImput">Name</label>
-              <input name="email" type="email" value={this.state.email} onChange={this.handleChange} className="form-control" id="emailImput" placeholder="email@domain.com" />
-            </div>
-            <input type="submit" value="Submit" className="btn btn-primary" />
-          </form>
-        </div>
-      )
-    }
-  }
+//     render() {
+//       return (
+//         <div>
+//           <form onSubmit={this.handleSubmit} >
+//             <div className="form-group">
+//               <label for="nameImput">Name</label>
+//               <input type="text" name="name" value={this.state.name} onChange={this.handleChange} className="form-control" id="nameImput" placeholder="Name" />
+//             </div>
+//             <div className="form-group">
+//               <label for="emailImput">Name</label>
+//               <input name="email" type="email" value={this.state.email} onChange={this.handleChange} className="form-control" id="emailImput" placeholder="email@domain.com" />
+//             </div>
+//             <input type="submit" value="Submit" className="btn btn-primary" />
+//           </form>
+//         </div>
+//       )
+//     }
+//   }
   
-  class MainTitle extends React.Component {
-    render(){
-      return(
-        <h1>React Form example</h1>
-      )
-    }
-  }
+//   class MainTitle extends React.Component {
+//     render(){
+//       return(
+//         <h1>React Form example</h1>
+//       )
+//     }
+//   }
   
-  class App extends React.Component {
-    render(){
-      return(
-        <div>
-          <MainTitle/>
-          <ContactForm/>
-        </div>
-      )
-    }
-  }
+//   class App extends React.Component {
+//     render(){
+//       return(
+//         <div>
+//           <MainTitle/>
+//           <ContactForm/>
+//         </div>
+//       )
+//     }
+//   }
   
-  React.render(<App/>, document.getElementById('app'))
+//   React.render(<App/>, document.getElementById('app'))
---
+++ src/components/HourlyGraph/index.js
@@ -0,0 +1,31 @@
+// import React, {Component} from 'react';
+// import {Line} from "react-chartjs-2";
+// import "./HourlyGraph.css";
+
+// function HourlyGraph(props){
+//     return(
+//         <div className="hourly_graph" style={{position: "relative", width:600, height: 300}}>
+//             <Line
+//                 options={{
+//                     responsive: true,
+//                     scales: {
+//                         xAxes: [{
+//                             gridLines: {
+//                                 display:false
+//                             }
+//                         }],
+//                         yAxes: [{
+//                             gridLines: {
+//                                 display:false
+//                             }
+//                         }]
+//                     }
+//                 }}
+//                 data={props.current_data}
+//             />
+//         </div>
+//     );
+
+// }
+
+// export default HourlyGraph;
