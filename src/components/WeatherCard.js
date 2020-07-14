a2334b59e413a733a3554c661677708df78ceb42
scbonner <codingbootcamp918@gmail.com>
2020-07-14 19:57:32 -0400

    "updating changes"

---
+++ src/components/WeatherCard.js
@@ -0,0 +1,19 @@
+// import React from "react";
+// import moment from "moment";
+
+// export default ({ className, dateTime, main, temp, feelsTemp, icon }) => {
+//   return (
+//     <div
+//       className={`d-flex flex-col border rounded pd cursor-pointer ${className}`}
+//     >
+//       <h4 className="m-0">{moment(dateTime * 1000).format("ddd, DD")}</h4>
+//       <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="icon" />
+//       <div className="font-sm">{main}</div>
+//       <div className="d-flex mg-top">
+//         <span className="flex-grow-1 font-lg">{temp}</span>
+//         <span className="font-sm">{feelsTemp}</span>
+//       </div>
+//     </div>
+//   );
+// };
+
---
+++ src/components/WeatherPage.js
@@ -0,0 +1,10 @@
+// import React from "react";
+// import moment from "moment";
+
+// export default ({ dateTime }) => {
+//   return (
+//     <div className={`d-flex flex-col border rounded pd cursor-pointer`}>
+//       <h4 className="m-0">{moment(dateTime * 1000).format("ddd, DD")}</h4>
+//     </div>
+//   );
+// };
