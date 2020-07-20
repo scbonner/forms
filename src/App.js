b0436f31ffec43a6b1ead2056c2baa7713aca3a1
scbonner <codingbootcamp918@gmail.com>
2020-07-20 02:42:07 -0400

    "updating changes"

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import React from 'react';
-// import App from './App'
 
 
-// // function Welcome(props) {
-// //   return <h1>Hello, {props.name}</h1>
 
-// }
-
-function HighSchoolSeniors(props) {
-  return <h1>Congrats! {props.name} you finished!</h1>
-}
-function App() {
-  return (
-    <div>
-      <HighSchoolSeniors name="Sara" />
-      <HighSchoolSeniors name="David" />
-      <HighSchoolSeniors name="Cahal" />
-      <HighSchoolSeniors name="Edite" />
-
-    </div>
-  )
-}
-
+const list = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: "Redux",
+      url: "https://reactjs.org/",
+      author: "Dan Abravov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ];
+  
+  const App = () => {
+    
+    return (
+      <div>
+        <h1>My Hacker Stories</h1>
+  
+        <label htmlFor="search">Search: </label>
+        <input 
+        id="search" 
+        type="text"
+        />
+  
+        <hr />
+  
+        {list.map(function(item) {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <br />
+               <span>{item.author}</span>
+              <br />
+  
+               <span>{item.num_comments}</span>
+              <br />
+  
+               <span>{item.points}</span> 
+              </div>
+              <br />
+              );
+    
+        }
+            
 
 export default App
-  
